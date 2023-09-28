fcada6ecc222e5b3690470fc5195a881994f4d17
dianabreik <diana.sam341@gmail.com>
2023-09-29 00:20:48 +0200

    Add an input button to search for a specific character by name

--- src/CharacterGallery.tsx
+++ src/CharacterGallery.tsx
@@ -1,16 +1,23 @@
 import {Character} from "./characters.ts";
 import CharacterCard from "./CharacterCard.tsx";
+import {ChangeEvent, useState} from "react";
 
 type CharacterGalleryProps = {
     characters : Character[]
 }
 export default function CharacterGallery(props : CharacterGalleryProps){
 
+    const [searchText,setSearchText] =useState<string>("")
 
+    const filteredCharacters = props.characters.filter((character)=> character.name.toLowerCase().includes(searchText.toLowerCase()))
+    function onSearchTextChange(event:ChangeEvent<HTMLInputElement>){
+        setSearchText(event.target.value)
+    }
     return (
              <div>
-                 <p>All Characters</p><br/>
-                 {props.characters.map((character)=><CharacterCard
+                 <p>Gallery</p><br/>
+                 <input onChange={onSearchTextChange} value={searchText}/>
+                 {filteredCharacters.map((character)=><CharacterCard
                                                                       key={character.id}
                                                                       character={character}/>)}
              </div>
